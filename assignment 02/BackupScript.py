1f264742568ba510594abac6da487300e6593797
montaser <montaser@kali.mont-kali>
2021-07-06 08:56:51 -0400

    first commit

--- assignment 02/BackupScript.py
+++ assignment 02/BackupScript.py
@@ -4,9 +4,9 @@ import GoogleDrive
 import slack
 
 # ssh server info
-host = 'host'
-username = 'username'
-password = 'password'
+host = 'add host here!'
+username = 'add username here!'
+password = 'add password here!'
 
 # ssh opertions
 ssh_opertions = ssh.ssh_session(host, username, password)
@@ -14,7 +14,7 @@ file_password = ssh_opertions.run()
 
 # upload the file to google drive
 my_drive = GoogleDrive.GDrive()
-Gdrive_folder_id = 'Google drive folder id'
+Gdrive_folder_id = 'add Google drive folder id here!'
 file_path = os.path.expanduser('~/')
 file_name = os.popen(f'ls {file_path} | grep BACKUP').read().strip()
 my_drive.upload_file(Gdrive_folder_id, file_name, file_path)
@@ -23,7 +23,7 @@ my_drive.upload_file(Gdrive_folder_id, file_name, file_path)
 os.popen(f'rm -rf ~/{file_name}')
 
 # send the password to Slack
-url = 'Slack Webhook URL'
+url = 'add Slack Webhook URL here!'
 message = f'{file_name} --> {file_password}'
 slack_message = slack.SendMessage(url, message)
 slack_message.send()
